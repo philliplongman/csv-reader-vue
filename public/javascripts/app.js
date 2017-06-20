76013ef09eebe50b52d13a4cdc62fef73ca0f9f7
Phillip Longman <himself@philliplongman.com>
2017-06-20 16:31:26 -0400

    Submit files automatically via AJAX and update the table.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,35 @@
-alert("It works!")
+function submitFile() {
+  var input = $("#upload-file")[0]
+  var file = input.files[0]
+
+  var data = new FormData()
+  data.append("file", file, file.name)
+
+  $.ajax({
+    method: "POST",
+    url: "/",
+    data: data,
+    processData: false,
+    contentType: false,
+    success: updateTable
+  })
+}
+
+function updateTable(data) {
+  var $body = $("table tbody")
+
+  $body.empty()
+
+  $.each(data.people, function(){
+    var $newRow = $("<tr>")
+
+    $newRow.append("<td>" + this.last_name + "</td>")
+    $newRow.append("<td>" + this.first_name + "</td>")
+    $newRow.append("<td>" + this.middle_initial + "</td>")
+    $newRow.append("<td>" + this.pet + "</td>")
+    $newRow.append("<td>" + this.birthday + "</td>")
+    $newRow.append("<td>" + this.color + "</td>")
+
+    $body.append($newRow)
+  })
+}
